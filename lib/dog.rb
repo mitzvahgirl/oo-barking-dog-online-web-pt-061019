5acfd28eae3adc4a2c61c444818c9bbb73d51080
Amy Fogelman <abfnj1@gmail.com>
2019-07-08 17:55:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,14 @@
 # Your code goes here!
 class Dog
-  def name=()
-    
-    
-    bark
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+ end
+ 
+ def name
+   @this_dogs_name
+ end  
+   
+def bark
     puts "woof!"
   end
 end
